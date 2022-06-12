997d974fa5a1ec9f727cb803d0876babf2a15e99
Pham Huu Hien <phhien203@gmail.com>
2022-06-12 08:55:22 +0700

    Infinite rerender example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
-import { useStorage } from "./hooks/useStorage";
+import { usePicture } from "./usePicture";
 
 function App() {
-  const [count, setCount] = useStorage("count", 0);
+  const [date, setDate] = useState(`2020-06-09`);
+  const { picture, loading } = usePicture(date);
 
-  return <div onClick={() => setCount(count + 1)}>The count is: {count}</div>;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <input
+        type="date"
+        name="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <h2>{picture.title}</h2>
+      <p>{picture.explanation}</p>
+      <img src={picture.url} alt={picture.title} />
+    </div>
+  );
 }
 
 export default App;
--- src/usePicture.js
+++ src/usePicture.js
@@ -1,9 +1,15 @@
+import { useMemo } from "react";
 import { useNetwork } from "./hooks/useNetwork";
 
 export const usePicture = (date) => {
-  const { data, loading } = useNetwork({
-    url: `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`,
-  });
+  const requestOptions = useMemo(
+    () => ({
+      url: `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`,
+    }),
+    [date]
+  );
+
+  const { data, loading } = useNetwork(requestOptions);
 
   return { picture: data, loading };
 };
